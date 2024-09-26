6ff848a4c634205adb040b2b2fdabdd467c441ba
pierrebressy <pierre.bressy@heig-vd.ch>
2024-09-26 08:36:54 +0000

    update 20240926

---
+++ TD20240920/my_algo_xyz.md
@@ -0,0 +1,16 @@
+# description de l'algorithme XYZ
+
+## objecitfs
+- objectif 1
+- objectif 2
+
+# descpription
+- description 1
+- description 2
+
+# algo
+- il faut multiplier i par j
+- il faut diviser i par j
+
+le résultat est : 42
+
--- TD20240920/td.c
+++ TD20240920/td.c
@@ -1,7 +1,26 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 int main(int argc, const char *argv[])
 {
+	// calcul racine carrée de i en utilisant l'algorithme de Johns & Smith 
+	// cf my_algo_xyz.md
+	/*	
+	a = 2^i;
+	i <<= 3;
+	i *= j;
+	j |= 0x32;
+	*/
+	
 	puts("dobar dan mozete raditi ?"); 
-	return 0;
-}
+	puts("Hello !"); 
+	puts("Bonjour !"); 
+	puts("Guten tag !"); 
+	puts("Good morning !"); 
+	puts("Bom dia !"); 
+	puts("Buenos dias !"); 
+	puts("Buongiorno !");
+	puts("Dzien dobry !");
+	
+	return 0; //0...255 
+
+} 
